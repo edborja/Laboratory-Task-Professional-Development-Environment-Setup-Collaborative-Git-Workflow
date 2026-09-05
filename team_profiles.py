8a11075fd9849ea1b733a24fb8abba94b9b1efd8
nonoelee-ui <nonoelee@my.cspc.edu.ph>
2026-09-05 23:55:06 +0800

    pfp

--- team_profiles.py
+++ team_profiles.py
@@ -62,7 +62,7 @@ def get_initial_team() -> List[TeamMember]:
             github_handle="@nonoelee-ui",          # example: "@jdelacruz-cspc"
             avatar_icon=ft.Icons.DATA_OBJECT,
             accent_color=ft.Colors.AMBER_800,
-            photo_url="/dev2.jpg",                 # Photo: local asset or URL (e.g. "https://github.com/dev2-github.png")
+            photo_url="assets/._dev2.jpg",                 # Photo: local asset or URL (e.g. "https://github.com/dev2-github.png")
         ),
         # ----------------------------------------------------------------------
         # TODO: [DEVELOPER 3 TASK] - Branch: feature/dev3-features
